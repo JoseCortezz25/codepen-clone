46fde2026c0d7c0a94e6133be08d095f3d079ed1
Alfonso Chavarro <alfonsocortezz25@gmail.com>
2023-05-14 18:07:40 -0500

    📦 NEW: Added Monaco Editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import Split from 'react-split-grid'
 import { encode, decode } from 'js-base64'
 import htmlIcon from './assets/html-logo.png'
 import cssIcon from './assets/css-logo.png'
 import javascriptIcon from './assets/javascript-logo.png'
+import { Editor } from '@monaco-editor/react';
+import { OnChange } from '@monaco-editor/react'
 import './App.css'
 
 type TemplateProps = {
@@ -13,40 +15,58 @@ type TemplateProps = {
 }
 
 function App() {
-  const html = useRef<HTMLTextAreaElement>(null)
-  const css = useRef<HTMLTextAreaElement>(null)
-  const js = useRef<HTMLTextAreaElement>(null)
+  const [html, setHtml] = useState<string>('')
+  const [css, setCss] = useState<string>('')
+  const [js, setJs] = useState<string>('')
   const [template, setTemplate] = useState('')
+
+  const COMMON_EDITOR_OPTIONS = {
+    automaticLayout: true,
+    fontSize: 18,
+
+  }
 
   useEffect(() => {
     const { pathname } = window.location;
+
     const [rawHtml, rawCss, rawJs] = pathname.slice(1).split('%7C')
 
-    const pureHtml = rawHtml === undefined ? decode(rawHtml) : ''
-    const pureCss = rawHtml === undefined ? decode(rawCss) : ''
-    const pureJs = rawHtml === undefined ? decode(rawJs) : ''
+    const pureHtml = rawHtml !== undefined ? decode(rawHtml) : ''
+    const pureCss = rawCss !== undefined ? decode(rawCss) : ''
+    const pureJs = rawJs !== undefined ? decode(rawJs) : ''
 
-    if (html.current !== null && css.current !== null && js.current !== null) {
-      html.current.value = pureHtml
-      css.current.value = pureCss
-      js.current.value = pureJs
+    if (html !== null && css !== null && js !== null) {
+      setHtml(pureHtml)
+      setCss(pureCss)
+      setJs(pureJs)
     }
 
-    const htmlForPreview = createHtml({ htmlValue: html.current?.value, cssValue: css.current?.value, jsValue: js.current?.value })
+    const htmlForPreview = createHtml({ htmlValue: pureHtml, cssValue: pureCss, jsValue: pureJs })
     setTemplate(htmlForPreview)
   }, [])
 
-  const update = () => {
-    const htmlValue = html.current?.value;
-    const cssValue = css.current?.value;
-    const jsValue = js.current?.value;
+  const handleEditorHtml: OnChange = (value) => {
+    setHtml(value)
+    update()
+  }
 
-    if (typeof htmlValue !== 'undefined' && typeof cssValue !== 'undefined' && typeof jsValue !== 'undefined') {
-      const hashedCode = `${encode(htmlValue)}|${encode(cssValue)}|${encode(jsValue)}`
+  const handleEditorCss: OnChange = (value) => {
+    setCss(value)
+    update()
+  }
+
+  const handleEditorJs: OnChange = (value) => {
+    setJs(value)
+    update()
+  }
+
+  const update = () => {
+    if (typeof html !== 'undefined' && typeof css !== 'undefined' && typeof js !== 'undefined') {
+      const hashedCode = `${encode(html)}|${encode(css)}|${encode(js)}`
       window.history.replaceState(null, '', `/${hashedCode}`)
     }
 
-    const htmlForPreview = createHtml({ htmlValue, cssValue, jsValue })
+    const htmlForPreview = createHtml({ htmlValue: html, cssValue: css, jsValue: js })
     setTemplate(htmlForPreview)
   }
 
@@ -82,16 +102,40 @@ function App() {
         }) => (
           <div className={"grid-container"} {...getGridProps()}>
             <div className={"item grid-item item-block1"}>
-              <textarea name="" id="" ref={html} onInput={update}></textarea>
-              <img alt="Icono de" src={htmlIcon} />
+              <Editor
+                defaultValue={html}
+                defaultLanguage="html"
+                onChange={handleEditorHtml}
+                theme='vs-dark'
+                options={{
+                  ...COMMON_EDITOR_OPTIONS
+                }}
+              />
+              <img alt="Icono de HTML" src={htmlIcon} />
             </div>
             <div className={"item grid-item item-block2"}>
-              <textarea name="" id="" ref={css} onInput={update}></textarea>
-              <img alt="Icono de" src={cssIcon} />
+              <Editor
+                defaultValue={css}
+                defaultLanguage="css"
+                onChange={handleEditorCss}
+                theme='vs-dark'
+                options={{
+                  ...COMMON_EDITOR_OPTIONS
+                }}
+              />
+              <img alt="Icono de CSS" src={cssIcon} />
             </div>
             <div className={"item grid-item item-block3"}>
-              <textarea name="" id="" ref={js} onInput={update}></textarea>
-              <img alt="Icono de" src={javascriptIcon} />
+              <Editor
+                defaultValue={js}
+                defaultLanguage="javascript"
+                onChange={handleEditorJs}
+                theme='vs-dark'
+                options={{
+                  ...COMMON_EDITOR_OPTIONS
+                }}
+              />
+              <img alt="Icono de JS" src={javascriptIcon} />
             </div>
             <div className={"grid-item item-block4"}>
               <iframe srcDoc={template}></iframe>
@@ -101,11 +145,6 @@ function App() {
               className={"grid-gutter item-horizontal gutter-horizontal"}
               {...getGutterProps("column", 1)}
             />
-
-            {/* <div
-              className={"grid-gutter item-horizontal2 gutter-horizontal"}
-              {...getGutterProps("column", 3)}
-            /> */}
 
             <div
               className={"grid-gutter item-vertical gutter-vertical"}
@@ -118,5 +157,6 @@ function App() {
     </main >
   )
 }
+
 
 export default App
